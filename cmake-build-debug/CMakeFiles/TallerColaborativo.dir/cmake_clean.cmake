b2ba4ea5a4f9a2c7652fe55885a5e64d6b77bdbf
SergioMesa2002 <sergioandresmesabenitez@gmail.com>
2023-07-08 21:22:37 -0500

    agregado ejercisio 1

---
+++ cmake-build-debug/CMakeFiles/TallerColaborativo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TallerColaborativo.dir/main.c.obj"
+  "CMakeFiles/TallerColaborativo.dir/main.c.obj.d"
+  "TallerColaborativo.exe"
+  "TallerColaborativo.exe.manifest"
+  "TallerColaborativo.pdb"
+  "libTallerColaborativo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TallerColaborativo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
